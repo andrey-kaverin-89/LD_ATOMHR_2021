da760a93943307ce2c9b1a33e95804055e2490d6
andrey-kaverin-89 <76730043+andrey-kaverin-89@users.noreply.github.com>
2021-09-26 00:37:37 +0300

    Add files via upload

---
+++ back/atomhr.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Employee (
+id int,
+position varchar(255),
+gender varchar(255),
+marital_status varchar(255),
+job_stop int,
+absence varchar(255),
+absence_days int,
+salary int,
+city varchar(255),
+children_num int,
+age int,
+has_mentor int,
+experience int,
+job_start_month int,
+layoff_month int,
+is_large_family int,
+is_retiree int);
