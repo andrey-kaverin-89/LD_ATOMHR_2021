fd41894236d75c5a9d560a51ac7c58e4b7bb6396
andrey-kaverin-89 <76730043+andrey-kaverin-89@users.noreply.github.com>
2021-09-26 00:47:14 +0300

    Add files via upload

--- back/atomhr_orm.py
+++ back/atomhr_orm.py
@@ -1,5 +1,5 @@
 import time
-from atomhr_config import db
+from atom_config import db
 
 class User(db.Model):
     user_id         = db.Column(db.Integer, primary_key=True)
@@ -23,3 +23,24 @@ class User(db.Model):
        }
     def __repr__(self):
         return '<User %r - %r>' % (self.username, self.email)
+
+class Employee(db.Model):
+    id              = db.Column(db.Integer, primary_key=True)
+    position        = db.Column(db.String(255), unique=False, nullable=True)
+    gender          = db.Column(db.String(255), unique=False, nullable=True)
+    marital_status  = db.Column(db.String(255), unique=False, nullable=True)
+    job_stop        = db.Column(db.Integer, unique=False, nullable=False)
+    absence         = db.Column(db.String(255), unique=False, nullable=True)
+    absence_days    = db.Column(db.Integer, unique=False, nullable=False)
+    salary          = db.Column(db.Integer, unique=False, nullable=False)
+    city            = db.Column(db.String(255), unique=False, nullable=True)
+    children_num    = db.Column(db.Integer, unique=False, nullable=False)
+    age             = db.Column(db.Integer, unique=False, nullable=False)
+    has_mentor      = db.Column(db.Integer, unique=False, nullable=False)
+    experience      = db.Column(db.Integer, unique=False, nullable=False)
+    job_start_month = db.Column(db.Integer, unique=False, nullable=False)
+    layoff_month    = db.Column(db.Integer, unique=False, nullable=False)
+    is_large_family = db.Column(db.Integer, unique=False, nullable=False)
+    is_retiree      = db.Column(db.Integer, unique=False, nullable=False)
+    def __repr__(self):
+        return '<Employee %r>' % self.id
