3d0778b2e322a47496ca43c4f0e19a1fc69486ca
andrey-kaverin-89 <76730043+andrey-kaverin-89@users.noreply.github.com>
2021-09-26 03:43:12 +0300

    Add files via upload

--- back/atomhr_config.py
+++ back/atomhr_config.py
@@ -3,14 +3,33 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
+from speechpro.cloud.speech.recognition import RecognitionClient
+from speechpro.cloud.speech.recognition import enums
+
 import datetime
+import pymorphy2
+
+morph = pymorphy2.MorphAnalyzer()
+
+# CRT
+username = "////////////" # Please enter your creds to speech kit
+password = "////////////" # Please enter your creds to speech kit
+domain_id = 42 # Please enter your creds to speech kit
+client = RecognitionClient(username, domain_id, password)
+config = {
+    'language': enums.Language.RU,
+    'model': enums.Model.GENERAL,
+    'encoding': enums.AudioEncoding.OGG_OPUS,
+    'response_type': enums.ResponseType.PLAIN_TEXT
+}
 
 # Flask etc
 app = Flask(__name__)
 app.config['DEBUG'] = True
 # app.config['DEBUG'] = False
 app.config["JWT_SECRET_KEY"] = "secretkey"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/atom_test.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/atom_test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name' # change to real mysql creds
 app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=1)
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 36000
 
